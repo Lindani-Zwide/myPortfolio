4bf9548d0d112b00b78fa72132118ed0eeef319f
Lindani-Zwide <222034990@stu.ukzn.ac.za>
2025-01-14 21:20:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ const toggleNaviBar=function(){
   if(!isNavBarOpen){
       naviBar.style.right='0'
       overLayEl.style.display='block'
-      toggleNavBarBtn.innerHTML=`<img src="/Assets/Icons/close-menu.png" alt="open navigation bar icon">`
+      toggleNavBarBtn.innerHTML=`<img src="Assets/close-menu.png" alt="open navigation bar icon">`
       isNavBarOpen=true
   }else{
     naviBar.style.right='-100%'
     overLayEl.style.display='none'
-    toggleNavBarBtn.innerHTML=`<img src="/Assets/Icons/open-menu.png" alt="open navigation bar icon">`
+    toggleNavBarBtn.innerHTML=`<img src="Assets/open-menu.png" alt="open navigation bar icon">`
     isNavBarOpen=false
   }
 }
@@ -30,4 +30,4 @@ navElsList.forEach((li)=>{
 overLayEl.addEventListener('click',toggleNaviBar)
 
 const imageEl=document.getElementById('image-el')
-imageEl.innerHTML=` <img src="/Assets/images/image.jpg" alt="Photo of Lindani Ndwandwe" width="140" height="140">`
+imageEl.innerHTML=` <img src="Assets/image.jpg" alt="Photo of Lindani Ndwandwe" width="140" height="140">`
