f8e3fc4a0679b2cd5d77d77ed413ab5c86cc5547
Lindani-Zwide <222034990@stu.ukzn.ac.za>
2025-01-27 11:39:59 +0200

    Delete Assets directory

--- Assets/Prices_web_dev/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('button').addEventListener('click', function() {
-    // Scroll to pricing section
-    document.querySelector('#pricing').scrollIntoView({ behavior: 'smooth' });
-});
-document.getElementById('footer').innerHTML=   `
-    <p>&copy; ${(new Date()).getFullYear()} Lindani Mthembu</p>
-`
