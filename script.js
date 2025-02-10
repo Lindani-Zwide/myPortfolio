0159460aefeff0e501ed5401b2954d1b131348c7
Lindani-Zwide <222034990@stu.ukzn.ac.za>
2025-02-11 00:11:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//Responsiveness for a navigation bar
+const toggleNavBarBtn=document.getElementById('toggle-sideBar');
+let isNavBarOpen=false;
+const navBar=document.getElementById('navBarEl');
+const overLayEl=document.getElementById('overlay')
+
+async function toggleNavBar(){
+    if(!isNavBarOpen){
+        navBar.style.right='0';
+        overLayEl.style.display='block'
+        toggleNavBarBtn.innerHTML=`<img src="Assets/close (1).png" height="33" width="33"  alt="close navigation bar icon">`;
+        isNavBarOpen=true;
+    }else{
+        navBar.style.right='-100%';
+        overLayEl.style.display='none'
+        toggleNavBarBtn.innerHTML=`<img src="Assets/open-menu-bar.png"  height="41" width="41" alt="open navigation bar icon">`;
+        isNavBarOpen=false;
+    }
+}
+
+toggleNavBarBtn.addEventListener('click',toggleNavBar);
+
+const nav_li_list=document.querySelectorAll('.li-item')
+nav_li_list.forEach((a)=>{
+    a.addEventListener('click',toggleNavBar)
+})
+
+overLayEl.addEventListener('click',toggleNavBar)
+
+//styles and contents for the footer
+const footerEl=document.getElementById('footer')
+const date=new Date()
+const year=date.getFullYear()
+footerEl.innerHTML=`
+                <p id="copyRight">
+                   © ${year} Lindani Mthembu
+                </p>
+`
+const cpRight= document.getElementById('copyRight')
+const cpRightStyle=cpRight.style
+cpRightStyle.color='#e5e5ed'
+cpRightStyle.opacity='0.7'
+cpRightStyle.fontSize='0.7rem'
+
+//Responsiveness of the projects section for the horizontal scroll
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentSlideIndex = 0;
+updateSlide();
+prevButton.addEventListener('click', () => {
+  currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+  updateSlide();
+});
+
+nextButton.addEventListener('click', () => {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  updateSlide();
+});
+
+function updateSlide() {
+  slides.forEach((slide, index) => {
+    slide.style.display = index === currentSlideIndex ? 'flex' : 'none';
+  });
+}
